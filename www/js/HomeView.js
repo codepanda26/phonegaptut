015a1797dbe2457deb00ee4b02845f389643753f
jenf11 <jenfry11@gmail.com>
2014-11-19 20:50:49 +0100

    upto step 7 in tutorial

---
+++ www/js/HomeView.js
@@ -0,0 +1,21 @@
+var HomeView = function(store){
+
+    this.initialize = function(){
+        this.el = $('<div>');
+        this.el.on('keyup','.search-key',this.findByName);
+    }  ;
+
+    this.render = function(){
+        this.el.html(HomeView.template());
+        return this;
+    };
+     this.findByName = function(){
+         store.findByName($('.search-key').val(), function(employees){
+             $('.employee-list').html(HomeView.liTempate(employees)) ;
+         });
+     }  ;
+    this.initialize();
+
+}
+HomeView.template = Handlebars.compile($("#home-tpl").html());
+HomeView.liTempate = Handlebars.compile($("#employee-li-tpl").html());
