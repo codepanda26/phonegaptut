43f1fa56ce0d516b73103a4905f6296042bed7bd
jenf11 <jenfry11@gmail.com>
2014-11-19 21:44:11 +0100

    step 8

--- www/js/HomeView.js
+++ www/js/HomeView.js
@@ -12,6 +12,14 @@ var HomeView = function(store){
      this.findByName = function(){
          store.findByName($('.search-key').val(), function(employees){
              $('.employee-list').html(HomeView.liTempate(employees)) ;
+            if(self.iscroll){
+                console.log('Refresh iScroll');
+                self.iscroll.refresh();
+            }
+             else{
+                console.log('New iScrol');
+                self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+            }
          });
      }  ;
     this.initialize();
